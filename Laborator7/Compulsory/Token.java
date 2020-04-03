cd62304166d2fe08e90cba87eeafb535468cda88
StefanPopa02 <60078395+StefanPopa02@users.noreply.github.com>
2020-04-03 17:16:31 +0300

    Add files via upload

---
+++ Laborator7/Compulsory/Token.java
@@ -0,0 +1,15 @@
+public class Token {
+    private Integer number;
+
+    public Token(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+}
